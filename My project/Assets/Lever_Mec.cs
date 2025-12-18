91cd5a9ec089aa6c4ee8a89c9664c6e01188591a
Jorge A Fernandez <jfernandez7424@sdsu.edu>
2025-12-18 00:26:03 -0800

    Level 1 rework

--- My project/Assets/Lever_Mec.cs
+++ My project/Assets/Lever_Mec.cs
@@ -1,16 +1,70 @@
 using UnityEngine;
+using UnityEngine.UI;       // Remove if using TextMeshPro only
 
 public class Lever_Mec : MonoBehaviour
 {
     public bool pulled = false; 
-    void Start()
+    public Doorinteraction door;   // Drag the door with DoorInteraction here
+    public Text messageText;       // Can be the same UI text as the door
+
+   void start(){}
+    public string interactPrompt = "Press E to pull the lever.";
+
+    private bool isActivated = false;
+
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (!other.CompareTag("Player")) return;
+
+        if (!isActivated)
+        {
+            ShowMessage(interactPrompt);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                ActivateLever();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        ClearMessage();
+    }
+
+    private void ActivateLever()
+    {
+        isActivated = true;
+        ClearMessage();
+
+        if (door != null)
+        {
+            door.UnlockDoor();
+        }
+    }
+
+    private void ShowMessage(string msg)
     {
         
+        if (messageText != null)
+        {
+            messageText.text = msg;
+        }
+        else
+        {
+            Debug.Log(msg);
+        }
+    }
+    void Update(){}
+    // Update is called once per frame
+    private void ClearMessage()
+    {
+        
+        if (messageText != null)
+        {
+            messageText.text = "";
+        }
     }
 }
