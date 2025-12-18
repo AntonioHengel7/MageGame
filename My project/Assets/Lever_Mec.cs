7108d31a49cf4038f792ed3201383b939d7dcc8f
Jorge A Fernandez <jfernandez7424@sdsu.edu>
2025-12-17 19:50:30 -0800

    Level 1 torches and fog

---
+++ My project/Assets/Lever_Mec.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Lever_Mec : MonoBehaviour
+{
+    public bool pulled = false; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
